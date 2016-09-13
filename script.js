17a060104112c3323af09ed733806043af5eac27
gness1804 <graham.nessler@gmail.com>
2016-09-12 22:07:23 -0600

    completed assessment with Brenna

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+$(document).ready(function () {
 
+
+  var input = $(".new-item-form--name");
+  var submit = $(".new-item-form--submit");
+
+  submit.on("click", function (event) {
+    event.preventDefault();
+    var text = input.val();
+
+    // debugger;
+    $(".item-list").append(moreText(text));
+    input.val("");
+  });
+
+  input.on("keyup", function (key) {
+    var isItEmpty = (input.val() === "")
+    submit.attr("disabled", false);
+    if (input.val() === "") {
+      submit.attr("disabled", true);
+    }
+  });
+
+  function moreText(text){
+    return `
+    <section>
+      <p>${text}</p>
+      <button class="removeThis">Remove Item</button>
+    </section>`
+  }
+
+  // input.on("keyup", function (key) {
+  //   var isItEmpty = (input.val() === "")
+  //   submit.attr("disabled", isItEmpty);
+  // }
+
+}); //end of jQuery body
